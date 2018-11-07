a4b6bb38e8efa173dd09f819803379797d5480f6
Ionela Halcu <halcuionela@yahoo.com>
2018-11-07 22:49:57 +0200

    Create directories

--- support/bg-proc.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-while [ 1 ]; do
-    echo "Tick!"
-    sleep 1
-    echo "Tock!"
-    sleep 1
-done
--- support/it-s-a-trap.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-trap "echo IT\'s A TRAP!" SIGINT SIGTERM SIGQUIT
-
-while [ 1 ]; do
-    echo "Still running . . . "
-    sleep 2
-done
